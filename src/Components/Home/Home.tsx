cb8e1958ab3158573ef584079eb087dde27f8da9
marwan5050 <marwanabdalla2015@gmail.com>
2026-03-16 23:21:28 +0200

    feat: initial commit

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer/Footer";
+import Navbar from "./Navbar/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <Footer />
+
+      <Footer />
+    </>
+  );
+}
