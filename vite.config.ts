e88e2322d0c5b7368cb57ff6f501b64fca4115c3
FADYxp <ffady354@gmail.com>
2026-03-18 06:19:19 +0200

    <feat>(project-setup):set up the libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-
+import react from '@vitejs/plugin-react'
+import path from 'path/win32'
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
