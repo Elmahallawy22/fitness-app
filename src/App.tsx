53fbfec6d5f6b97a2a27e9efa00c6bc098c05013
ali amar <aliamarr033@gmail.com>
2026-03-21 18:00:17 +0200

    fix(font): correct @fontsource rubik import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import "./App.css";
 
 function App() {
-
   return (
     <>
-<div className="p-10 text-center font-sans">
-  <h1 className="text-6xl font-extrabold tracking-tight font-baloo">
-   test <span className="text-primary">FITNESS</span>
-  </h1>
-  <p className="text-xl mt-4 font-medium opacity-80">
-    test fonts
-  </p>
-</div>
+      <div className="p-10 text-center font-sans">
+        <h1 className="text-6xl font-extrabold tracking-tight font-baloo">
+          test <span className="text-primary">FITNESS</span>
+        </h1>
+        <p className="text-xl mt-4 font-medium opacity-80">test fonts</p>
+      </div>
     </>
   );
 }
