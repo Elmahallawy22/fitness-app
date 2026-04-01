148b49136b29b8212ea1a0345aeb8b0ad8fdfbb2
marwan5050 <marwanabdalla2015@gmail.com>
2026-04-02 01:57:26 +0200

    update(meal-page): work on ui component to match the figma

--- src/App/healthy/Healthy.tsx
+++ src/App/healthy/Healthy.tsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/Components/ui/carousel";
+} from "../../Components/ui/carousel";
+
+import vector from "../../assets/Images/Vector.png";
+import dumble from "../../assets/Images/dumble.png";
 
 type Category = {
   idCategory: string;
@@ -62,47 +64,90 @@ export default function Healthy() {
 
   return (
     <>
-      <div className="p-6 mt-24">
-        {/* ✅ Categories Slider */}
-        <Carousel className="mb-8 bg-blue-600 w-1/2 mx-auto ">
-          <CarouselContent>
-            {chunkedCategories.map((group, index) => (
-              <CarouselItem key={index}>
-                <div className="flex justify-center gap-6">
-                  {group.map((cat) => (
-                    <button
-                      key={cat.idCategory}
-                      onClick={() => setSelectedCategory(cat.strCategory)}
-                      className={`text-lg font-medium transition ${
-                        selectedCategory === cat.strCategory
-                          ? "text-orange-600"
-                          : "text-gray-500"
-                      }`}>
-                      {cat.strCategory}
-                    </button>
-                  ))}
-                </div>
+      <div className="my-main mt-48 bg-gradient-to-b from-main/20 via-main/5 to-main/90">
+        {/* healthy LAYER  */}
+        <h2
+          className="
+                  relative inline-block text-center bottom-12 -z-1 left-1/2 -translate-x-1/2  text-6xl font-bold
+                  bg-gradient-to-b from-white to-[#232425]
+                   bg-clip-text text-transparent">
+          HEALTHY
+        </h2>
 
-                {/* ✅ Meals Grid */}
-                <div className="bg-red-600 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-                  {meals.map((meal) => (
-                    <div key={meal.idMeal} className="rounded-lg border p-4">
-                      <img
-                        src={meal.strMealThumb}
-                        alt={meal.strMeal}
-                        className="mb-3 h-40 w-full rounded-md object-cover"
-                      />
-                      <h3 className="text-lg font-semibold">{meal.strMeal}</h3>
-                    </div>
-                  ))}
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
+        {/* header */}
+        <div>
+          {/* small header */}
+          <div className="flex justify-center items-center w-5xl mx-auto  gap-2 mb-4">
+            <img src={dumble} alt="img-dumble" className="w-9" />
+            <h4 className="text-orange-600">Healthy Nutritions</h4>
+          </div>
+          <h2 className="text-4xl text-center  font-bold uppercase leading-16  mb-10 ">
+            Fuel your fitness journey with <br />
+            customized <span className="text-orange-600"> meal plans </span>for
+            you
+          </h2>
+        </div>
 
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+        <div className="p-6 ">
+          {/* ✅ Categories Slider */}
+          <Carousel className="w-1/2 mx-auto ">
+            <CarouselContent>
+              {chunkedCategories.map((group, index) => (
+                <CarouselItem key={index}>
+                  <div className="mb-8 flex justify-center gap-6">
+                    {group.map((cat) => (
+                      <button
+                        key={cat.idCategory}
+                        onClick={() => setSelectedCategory(cat.strCategory)}
+                        className={`text-lg font-medium transition ${
+                          selectedCategory === cat.strCategory
+                            ? "text-orange-600"
+                            : "text-gray-500"
+                        }`}>
+                        {cat.strCategory}
+                      </button>
+                    ))}
+                  </div>
+
+                  {/* ✅ Meals Grid */}
+                  <div className=" grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                    {meals.map((meal) => (
+                      <div key={meal.idMeal} className="  border p-1">
+                        <img
+                          src={meal.strMealThumb}
+                          alt={meal.strMeal}
+                          className="  w-full rounded-md object-cover"
+                        />
+                        {/* slide content */}
+                        <div className="relative bottom-0 left-0 bg-gradient-to-r from-[#171E2E00]  via-[#171E2E80]   to-[#171E2ECC]   backdrop-blur-[3.75rem] p-4 gap-2">
+                          {/* meal title */}
+                          <h3 className=" mb-2  text-main dark:text-zinc-100  text-xl font-bold uppercase tracking-[0.14rem] leading-8">
+                            {meal.strMeal}
+                          </h3>
+
+                          {/* slide button */}
+                          <div className="flex">
+                            <button className="text-orange-600">Explore</button>
+                            <div className="p-2 w-6 h-6 ms-2 bg-orange-600 rounded-full">
+                              <img
+                                src={vector}
+                                alt="img-button"
+                                className="w-full text-center h-full"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
       </div>
     </>
   );
