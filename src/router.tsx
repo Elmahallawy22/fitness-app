89ffa1429468489c8fd8af849a4c0f5c967aa95e
Omar Eltantawy <oeltantawy8@gmail.com>
2026-04-23 13:00:46 +0200

    fix: fix conflicts

--- src/router.tsx
+++ src/router.tsx
@@ -10,8 +10,6 @@ import ForgotPassword from "./app/auth/forgot-password";
 import Healthy from "./app/healthy/components/page";
 import Login from "./app/auth/login";
 import Classes from "./app/classes/page";
-import Healthy from "./app/healthy/page";
-import Login from "./app/auth/login/Login";
 import Account from "./app/account/page";
 import AccountLayout from "./layouts/account-layout";
 import Meal from "./app/healthy/meals/[mealId]/page";
